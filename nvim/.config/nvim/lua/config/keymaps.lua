dc647edfe2b0bc6c70fdd69462b2230a604ee07c
Amrit Agarwal <agaamrit@amazon.com>
2023-11-05 22:53:55 +0000

    Add vim tmux navigator
    Add nvim tmux navigator plugin. Import/Add keymaps file.

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<c-h>", "<Cmd>TmuxNavigateLeft<CR>", { silent = true })
+vim.keymap.set("n", "<c-j>", "<Cmd>TmuxNavigatDown<CR>", { silent = true })
+vim.keymap.set("n", "<c-k>", "<Cmd>TmuxNavigateUp<CR>", { silent = true })
+vim.keymap.set("n", "<c-l>", "<Cmd>TmuxNavigateRight<CR>", { silent = true })
+vim.keymap.set("n", "<c-\\>", "<Cmd>TmuxNavigatePrevious<CR>", { silent = true })
