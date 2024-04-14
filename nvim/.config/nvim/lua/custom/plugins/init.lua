edc8f440065598034978ebbd7ea5b147ce2e722e
Amrit Agarwal <agaamrit@amazon.com>
2024-04-14 19:45:22 +0000

    Add neotree, lazygit, tmux-navigator and floaterm

--- nvim/.config/nvim/lua/custom/plugins/init.lua
+++ nvim/.config/nvim/lua/custom/plugins/init.lua
@@ -2,5 +2,50 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return { 'mfussenegger/nvim-jdtls',
-  'mfussenegger/nvim-dap'}
+return {
+  'mfussenegger/nvim-jdtls',
+  'mfussenegger/nvim-dap',
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    keys = {
+      { "<leader>ne", "<cmd>Neotree toggle<cr>",                 desc = "Neotree" },
+      { "<leader>nr", "<cmd>Neotree filesystem reveal left<cr>", desc = "Neotree" },
+    }
+  },
+  {
+    'voldikss/vim-floaterm',
+    event = "VeryLazy",
+    keys = {
+      { "<C-t>", "<cmd>FloatermToggle<cr>", desc = "Terminal" },
+    },
+  },
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    }
+  },
+  {
+    "christoomey/vim-tmux-navigator",
+  },
+}
